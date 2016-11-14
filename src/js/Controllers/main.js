0723084fee6f762ee9bdf773c3aa93d934d275e4
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-14 16:28:24 -0500

    added second button and made it work

--- src/js/Controllers/main.js
+++ src/js/Controllers/main.js
@@ -2,13 +2,24 @@ function MainController ($scope) {
   $scope.count = 0;
   $scope.string = "0 likes"
 
+  var countUp = true;
+
   $scope.addCount = function () {
-    let newCount = $scope.count = $scope.count + 1;
+    let newCount = (countUp) ? $scope.count++ : $scope.count--;
+
     if ($scope.count === 1) {
       $scope.string = $scope.count + " like"
     } else {
       $scope.string = $scope.count + " likes"
     }
+    if ($scope.count < 0) {
+      $scope.count = 0
+      $scope.string = "0 likes"
+    }
+  };
+
+  $scope.changeCount = function () {
+    countUp = !countUp;
   };
 
 };
