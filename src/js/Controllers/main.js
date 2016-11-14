5f45e65597b622b0ab8f89873b6c5436ce07bf7b
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-14 15:34:24 -0500

    added a string and got everything working as it should

--- src/js/Controllers/main.js
+++ src/js/Controllers/main.js
@@ -1,9 +1,14 @@
 function MainController ($scope) {
   $scope.count = 0;
+  $scope.string = "0 likes"
 
   $scope.addCount = function () {
     let newCount = $scope.count = $scope.count + 1;
-    console.log(newCount);
+    if ($scope.count === 1) {
+      $scope.string = $scope.count + " like"
+    } else {
+      $scope.string = $scope.count + " likes"
+    }
   };
 
 };
